204fd36378a025d4feee8a26d2de683cb85af114
JEAN GAL <j.c.r.gal@student.tudelft.nl>
2022-11-20 14:54:25 +0100

    unused import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import Header from './components/Header';
-import FamilyTree from './familytree';
 import FamilyTreePage from './pages/FamilyTree';
-import { useEffect } from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import About from './pages/About';
 
